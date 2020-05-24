bca0477e371f392ffb8a3920c7c87a0de0b5869b
uttesh <uttesh@rivetsys.com>
2020-05-24 22:36:02 +0530

    host pages

---
+++ precache-manifest.8a74a88f1ef685a0ce8ee06f63b93d2e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c291acaccc6628fbfc5216fd7693ec89",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3692ff8619a9aef3351f",
+    "url": "/static/css/main.36fbd697.chunk.css"
+  },
+  {
+    "revision": "d36448c41c74f48f3e6e",
+    "url": "/static/js/2.ba271a8d.chunk.js"
+  },
+  {
+    "revision": "6ef718eeb39c03c0fcdab1a60b0c6b00",
+    "url": "/static/js/2.ba271a8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3692ff8619a9aef3351f",
+    "url": "/static/js/main.f335c77d.chunk.js"
+  },
+  {
+    "revision": "01c18a8826793ec178e5",
+    "url": "/static/js/runtime-main.a37f0d04.js"
+  }
+]);
